9573ba0c6ce1c2f36d2c4eb23766047cadb35c45
ainneth <angie.ainneth.a@gmail.com>
2026-04-25 21:42:36 +0000

    added undo remove

--- mainList.java
+++ mainList.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.util.Stack;
+import java.util.Queue; // addings for future use if needed
+import java.util.LinkedList;
+
 class Task {
     private String description;
 
@@ -29,17 +33,36 @@ class Task {
 
 class TaskManager{
     private ArrayList<Task> tasks = new ArrayList<>();
+    private Stack<Task> undoStack = new Stack<>();
+    private Queue<Task> taskQueue = new LinkedList<>();
+
+
     
         public void addTask(String desc){
             tasks.add(new Task(desc));
+            taskQueue.add(new Task(desc));
         }
         public void removeTask(int index){
             if(index >= 0 && index < tasks.size()) {         // tasknumber has been switched to index
                     tasks.remove(index);                    //along with <= becoming just < and 
+                    undoStack.push(removed);
+                System.out.println( "Task removed successfully.");           
             } else {
                     System.out.println("Uh-oh! Invalid task number. Please try again.");
             }
         }
+
+    /// undo remobe tasks method as wel asn the process of the nect task method are here
+            public void undoRemove() {
+                if (!undoStack.isEmpty()) {
+                    Task restored = undoStack.pop();
+                    System.out.println("Task restored: " + restored);
+                }else {
+                    System.out.println( "Nothing to undo.")
+                }
+            }
+     }//end of undoRemove
+
 
         public void listTasks() {
             if (tasks.isEmpty()){
