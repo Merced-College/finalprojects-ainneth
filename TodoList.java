59ed3824aead753d13c436bc55a3025b409eb698
ainneth <angie.ainneth.a@gmail.com>
2026-04-25 04:53:45 +0000

    Added a switch method

--- TodoList.java
+++ TodoList.java
@@ -7,13 +7,35 @@ import java.util.Scanner;
 public class TodoList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
+        int option;
 
         System.out.println( "To-Do List Menu");
         System.out.println( "1. View Tasks");
         System.out.println("2. Add a Task");
         System.out.println("3. Remove a Task");
-        System.out.println("Choose and option: ");
+        System.out.println("Choose an option: ");
 
-    }
+        option = scanner.nextInt();
+        scanner.nextLine();
+
+        //Source: Java Switch Statements from W3Schools
+        //Modified by: Angie Alvarez
+
+        /* rather than using if else statements, the switch statement provides
+         a easier view of when a choice is made */
+        switch(option){
+            case 1:
+                System.out.println("Viewing tasks...");
+                break;
+            case 2:
+                System.out.println("Enter task: ");
+                break;
+            case 3:
+                System.out.println("Remove task:");
+                break;
+        }
+        }
+
+
 
 }
